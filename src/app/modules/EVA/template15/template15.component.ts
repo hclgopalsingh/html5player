7fb8c6a18f39b8951c0677c98a7363b38423d9d6
DeepChanderBhatt <deepchander.bhatt@hcl.com>
2020-05-27 15:20:30 +0530

    EVA global change: Disable next button on last ques

--- src/app/modules/EVA/template15/template15.component.ts
+++ src/app/modules/EVA/template15/template15.component.ts
@@ -126,7 +126,7 @@ export class Template15Component implements OnInit {
     }
 
     ngOnInit() {       
-        
+        this.Sharedservice.setLastQuesAageyBadheStatus(false);
         this.sprite.nativeElement.style="display:none";
         this.ifRightAns = false;
         this.attemptType = "";
