d04f228b85ebfa7f68dbf7f5ca9de8e2ebf7764d
Jyoti mishra <61728749+vijayjyotimishra@users.noreply.github.com>
2020-06-25 18:42:25 +0530

    Instruction audio multiple play issue fixes

--- src/app/controller/quesController.component.ts
+++ src/app/controller/quesController.component.ts
@@ -93,20 +93,25 @@ export class QuesController implements OnInit {
       console.log("selected question index", this.questionNo); 
     })
 
-      //*********  Move to next segment after 5 min of last question attempt */
-      this.Sharedservice.getTimerOnLastQues().subscribe(data => {
-        if (data.data && this.EVA) {
-          setTimeout(() => {
-            this.appModel.nextSectionEVA();
-          }, 5 * 60 * 1000);
-        }
-      });
+    
 
        
       // **** Disable aagey badhe button while on last question
     this.subscriptionControlAssets = this.appModel.getQuesControlAssets().subscribe(controlAssets => {
       this.quesCtrl = controlAssets;
       this.isLastQues = this.quesCtrl.isLastQues;
+
+
+        //*********  Move to next segment after 5 min of last question attempt */
+        this.Sharedservice.getTimerOnLastQues().subscribe(data => {
+          if (data.data && this.EVA) {
+            setTimeout(() => {
+              this.appModel.nextSectionEVA(this.isLastQues);
+            }, 5 * 60 * 1000);
+          }
+        });
+
+
       // **** Enable show answer button
       this.subscription = this.Sharedservice.getShowAnsEnabled().subscribe(data => { 
         this.EnableShowAnswer = data.data;
